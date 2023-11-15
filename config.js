5796401805394c096dce40ce8f4e72202b88c9b7
watchNeilsStuff <118492208+watchNeilsStuff@users.noreply.github.com>
2023-11-15 00:31:45 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://watchproxy.denkrishna.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTI2ODYyZWQ4NzhmNjNiNjY0NDRjZmQzZGRkN2EzNSIsInN1YiI6IjY1NTQ3MGExZWE4NGM3MTA5MjI3MjIxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gvCB9e8Mddcr9SLs3S6rkhVNXrAcHvsJ27FolK_xTfM"
+};
